c678f1fa70363876b51084bcff4523b3832ba7bc
potatoCompletion <033kws2628@gmail.com>
2023-02-15 17:24:57 +0900

    주석 수정

--- src/main/java/com/temp/api/common/Annotation/CurrentUser.java
+++ src/main/java/com/temp/api/common/Annotation/CurrentUser.java
@@ -7,12 +7,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// 런타임 까지 유지
 @Retention(RetentionPolicy.RUNTIME)
-// 타겟은 파라미터에만 붙이겠다.
 @Target(ElementType.PARAMETER)
-// 익명 사용자인 경우에는 null로, 익명 사용자가 아닌 경우에는 실제 account 객체로
-// Principal 을 다이나믹 하게 꺼내기 위해 @CurrentUser 생성
 @AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : userInfoEntity")
 public @interface CurrentUser {
 }
